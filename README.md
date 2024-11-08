779c925bc5ad127e6bcfa094aa03b0c5906d2824
kaotik <kaotik@localhost.localdomain>
2024-11-08 11:47:35 +0300

    Python is fun

---
+++ README.md
@@ -0,0 +1,1 @@
+#started learning python with plp
---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input ("What is your name? ")
+
+age = input ("How old are you? ")
+
+location = input("where do you live ")
+
+print(f"hello my name is {name} ,I am {age} years old and I live in {location}")
